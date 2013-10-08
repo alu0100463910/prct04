9371a77768a389efb79bdf85e0c4ef5571161f39
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-10-08 23:16:18 +0100

    Arreglo en suma matriz

--- src/main.rb
+++ src/main.rb
@@ -46,7 +46,7 @@ for i in 0...a.size do
 end
 
 #Mostrar Multiplicacion
-puts "\n"
+puts 
 puts "Multipliacion de Matrices"
 s.each do |x|
   puts x.join(" ")
@@ -56,12 +56,12 @@ end
 s[i][j]=0;
 for i in 0...a.size do
   for j in 0...b.size do
-    s[i][j] += a[i][j] * b[i][j]
+    s[i][j] = a[i][j] + b[i][j]
   end
 end
 
 #Mostrar Suma
-puts "\n"
+puts 
 puts "Suma de Matrices"
 s.each do |x|
   puts x.join(" ")
