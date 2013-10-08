685f448ea8ed27968f7dc543d88841dc3af78eb3
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-10-08 22:22:21 +0100

    Cambio expresion regular. Lectura correcta fichero.

--- src/main.rb
+++ src/main.rb
@@ -9,8 +9,8 @@ end
 
 def to_m(a)
   a = a.split(/\n/)
-  a = a.map { |r| r.split(/\s+/)}
-  a = mapmap(a) {|x| x.to_f}
+  a = a.map { |r| r.split(/[\s+\n]/)}
+  a = mapmap(a) {|x| x.to_i}
 end
 
 
@@ -29,18 +29,43 @@ a,b = texto.split(/\n\n+/)
 
 a = to_m(a)
 b = to_m(b)
+
 s = Array.new(b.size) { Array.new(a.size) }
 
+
+
 #Multiplicacion de matrices
-for i in (0...a.size)
-  for j in (0...b.size)
+
+for i in 0...a.size do
+  for j in 0...b.size do
     s[i][j]=0;
-    for k in (0...b.size)
-      s[i][j] = s[i][j] + a[i][k] * b[k][j]
+    for k in 0...b.size do
+      s[i][j] += a[i][k] * b[k][j]
     end
   end
 end
 
+#Mostrar Multiplicacion
+puts "\n"
+puts "Multipliacion de Matrices"
+s.each do |x|
+  puts x.join(" ")
+end
+
+#Suma de matrices
+s[i][j]=0;
+for i in 0...a.size do
+  for j in 0...b.size do
+    s[i][j] += a[i][j] * b[i][j]
+  end
+end
+
+#Mostrar Suma
+puts "\n"
+puts "Suma de Matrices"
+s.each do |x|
+  puts x.join(" ")
+end
 
       
 
