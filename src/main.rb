8c374d619cb8b0520de55862fd426b96f0de2ddd
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-10-08 12:48:54 +0100

    Actualizado main.rb(multiplicacion)  y LEAME.txt

--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,4 @@
-
+#!/usr/bin/ruby
 
 require matrix
 
@@ -13,12 +13,25 @@ def to_m(a)
   a = mapmap(a) {|x| x.to_f}
 end
 
-fichero = File.open(ARGV[0])
+fichero = File.open('cadena')
 texto = fichero.read
 
-a,b = texto.split(/\n\n*/)   // devuelve un string de 1º y 2º matriz
+a,b = texto.split(/\n\n*/)         
 
 a = to_m(a)
 b = to_m(b)
+
+
+#Multiplicacion de matrices
+for i in (0...a.size)
+  for j in (0...b.size)
+    a[i][j]=0;
+    for k in (0...b.size)
+      a[i][j] += a[i][k] * b[k][j]
+    end
+  end
+end
+      
+
 
 
